b1c0f59b847dff615a497220c499ab4a440850e5
Devon Kearns <dookie@kali.org>
2012-12-29 07:51:26 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sfuzz):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sfuzz" command="/usr/bin/sfuzz"
--- debian/sfuzz.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sfuzz package
-#
-0 4	* * *	root	[ -x /usr/bin/sfuzz_maintenance ] && /usr/bin/sfuzz_maintenance
